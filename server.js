41e07317a78fbde6ada1166ab16c04ccaa058f04
sachinikaushalyamaduwanthi <20663642@students.ltu.edu.au>
2023-08-19 05:16:23 -0400

    DATE and FORTUNE COOKIE MESSAGES added successfully

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const child_process = require('child_process');
 const app = express();
 // Specify how to respond to GET /, // Set handler for the index of the website
 app.get('/', (req, res) => {
+// Get the current date and time
+const currentDate = new Date().toLocaleString();
 // Run the system `fortune` command and respond with the message
 child_process.exec('fortune', (error, message) => {
 if(error === null) {
-res.send(message);
+//const response = `Current Date and Time: ${currentDate}\nFortune Message: ${message}`;
+//            res.send(response);
+const response = 'Current Date and Time: ' + currentDate +'<br>'+ 'Fortune Message: ' + message;
+            res.send(response);
 } else {
 res.send('Error: ' + error);
 }
